0331e287b6457cc924dc2339f65ec421dd2d83a5
roman4i <verb.r17@gmail.com>
2021-02-07 15:12:51 +0200

    Event page added

---
+++ src/event-Create/eventCreatePage.js
@@ -0,0 +1,75 @@
+export default function createEventPage(navFunc, routeArr) {
+  const eventDiv = document.createElement('div');
+  eventDiv.id = 'createEventDiv';
+  eventDiv.classList = 'eventDivStyle';
+
+  const eventNameDiv = document.createElement('div');
+  eventNameDiv.classList = 'eventOptionDiv';
+
+  const nameDivCont = document.createElement('div');
+  nameDivCont.insertAdjacentText('afterbegin', 'Name of the event:');
+  nameDivCont.classList = 'optionText';
+  eventNameDiv.append(nameDivCont);
+
+  const nameInput = document.createElement('input');
+  nameInput.type = 'text';
+  nameInput.classList = 'selectionEvent';
+  eventNameDiv.append(nameInput);
+
+  eventDiv.append(eventNameDiv);
+
+  function createEventOption(text, optionId, data, multiple) {
+    const optionDiv = document.createElement('div');
+    optionDiv.classList = 'eventOptionDiv';
+
+    const optionTextDiv = document.createElement('div');
+    optionTextDiv.insertAdjacentText('afterbegin', text);
+    optionTextDiv.classList = 'optionText';
+    optionDiv.append(optionTextDiv);
+
+    const optionSelection = document.createElement('select');
+    optionSelection.id = optionId;
+    optionSelection.multiple = multiple;
+    optionSelection.classList = 'selectionEvent';
+
+    data.forEach((item, number) => {
+      const option = document.createElement('option');
+      option.value = number;
+      option.text = item;
+      optionSelection.add(option);
+    });
+
+    optionDiv.append(optionSelection);
+    eventDiv.append(optionDiv);
+  }
+
+  const personName = ['Igor', 'Oleg', 'Olga', 'Yaroslav', 'Anna'];
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+  const timeList = ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'];
+  createEventOption('Participants:', 'choosePerson', personName, true);
+  createEventOption('Day:', 'daySelect', days, false);
+  createEventOption('Time:', 'timeSelect', timeList, false);
+
+  const buttonsDiv = document.createElement('div');
+  buttonsDiv.classList = 'buttonsStyle';
+
+  const cancelBut = document.createElement('input');
+  cancelBut.type = 'button';
+  cancelBut.id = 'cancelEventCreate';
+  cancelBut.value = 'Cancel';
+  cancelBut.classList = 'eventButton cancelButton';
+  cancelBut.onclick = () => {
+    navFunc('/calendar', routeArr);
+    eventDiv.remove();
+  };
+  buttonsDiv.append(cancelBut);
+
+  const createBut = document.createElement('input');
+  createBut.type = 'button';
+  createBut.value = 'Create';
+  createBut.classList = 'eventButton';
+  buttonsDiv.append(createBut);
+
+  eventDiv.append(buttonsDiv);
+  document.getElementById('root').append(eventDiv);
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import navigation from './navigation/navigation';
 import createCalendarPage from './calendar/calendar';
+import createEventPage from './event-Create/eventCreatePage';
 
 import './calendar/head/calendarHead.scss';
 import './calendar/table/tableStyle.scss';
+import './event-Create/eventPage.scss';
 
 const routes = {
   '/calendar': createCalendarPage,
-  '/create-event': createEventPage,
+  '/create-event': () => {
+    createEventPage(navigation, routes);
+  },
 };
 
-document.body.onload = () => {
-  navigation('/calendar', routes);
-};
+navigation('/calendar', routes);
 
 document.getElementById('createEvent').onclick = () => {
+  document.getElementById('calendarContainer').remove();
+  document.getElementById('tableCont').remove();
   navigation('/create-event', routes);
 };
